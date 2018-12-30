c98f9120c0f630d3d1dc0774cb0540cb8a2c9c8c
Istvan <istvan@lambdainsight.com>
2018-12-30 23:29:43 +0100

    Adding logging to gofast

---
+++ gofast/gofast.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	addr     = flag.String("addr", ":8080", "TCP address to listen to")
+	compress = flag.Bool("compress", false, "Whether to enable transparent response compression")
+	Trace    *log.Logger
+	Info     *log.Logger
+	Warning  *log.Logger
+	Error    *log.Logger
+)
+
+func Init(
+	traceHandle io.Writer,
+	infoHandle io.Writer,
+	warningHandle io.Writer,
+	errorHandle io.Writer) {
+
+	Trace = log.New(traceHandle,
+		"TRACE: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Info = log.New(infoHandle,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Warning = log.New(warningHandle,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Error = log.New(errorHandle,
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func main() {
+	Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+
+	flag.Parse()
+
+	h := requestHandler
+
+	Info.Println("Lofasz is starting on http://localhost:8080")
+
+	if *compress {
+		h = fasthttp.CompressHandler(h)
+	}
+
+	if err := fasthttp.ListenAndServe(*addr, h); err != nil {
+		log.Fatalf("Error in ListenAndServe: %s", err)
+	}
+}
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "/\n")
+	ctx.SetContentType("text/plain; charset=utf8")
+}
